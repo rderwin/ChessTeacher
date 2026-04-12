234a2534b453adc2cf512c09e605850239eb834f
rderwin <5152283+rderwin@users.noreply.github.com>
2026-04-12 10:16:10 -0700

    Register Modern Benoni and Grunfeld Defense in openings index

--- src/data/openings/index.ts
+++ src/data/openings/index.ts
@@ -1,9 +1,11 @@
 import { OpeningLine } from "../types";
+import { modernBenoni } from "./benoni";
 import { caroKann } from "./caro-kann";
 import { catalan } from "./catalan";
 import { dutchDefense } from "./dutch-defense";
 import { englishOpening } from "./english-opening";
 import { frenchDefense } from "./french-defense";
+import { grunfeld } from "./grunfeld";
 import { italianGame } from "./italian-game";
 import { kingsIndian } from "./kings-indian";
 import { londonSystem } from "./london-system";
@@ -16,10 +18,12 @@ import { scotchGame } from "./scotch-game";
 import { slavDefense } from "./slav-defense";
 
 export const ALL_OPENINGS: OpeningLine[] = [
+  modernBenoni,
   caroKann,
   catalan,
   dutchDefense,
   englishOpening,
+  grunfeld,
   italianGame,
   scotchGame,
   frenchDefense,
